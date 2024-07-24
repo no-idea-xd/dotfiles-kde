7ac16f520b147f14656396a7405e1768267bba61
no idea <155023962+no-idea-xd@users.noreply.github.com>
2024-07-24 19:25:42 +0200

    added keybindings

---
+++ config/nvim/lua/plugins/bindings.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.mapping = vim.tbl_deep_extend("force", opts.mapping, {
+        ["<tab>"] = cmp.mapping.confirm({ select = true }),
+        ["<CR>"] = cmp.config.disable,
+        ["<Del>"] = cmp.mapping.abort(),
+      })
+    end,
+  },
+}
